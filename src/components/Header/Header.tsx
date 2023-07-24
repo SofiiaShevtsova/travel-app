cc249430cdb5e559785b97ed2dced62acde42bbe
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-07-24 21:17:57 +0300

    + header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
+import Logo from "./Logo"
+import LinkIcon from "../Links/LinkIcon"
+import DropDownMenu from "../DropDown/DropDownMenu"
+import ButtonIcon from "../Button/ButtonIcon"
+import Menu from "../DropDown/Menu"
+
 const Header = () => {
-    return(<></>)
+    return (<div>
+        <Logo />
+        <LinkIcon path="/booking" icon={}/>
+        <DropDownMenu button={<ButtonIcon icon={} onClick={()=>{}}/>} menu={<Menu/>}/>
+    </div>)
 }
 
 export default Header
