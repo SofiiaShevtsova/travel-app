28f466ed3616bf147e7878ca85ff05c101847fdb
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-07-24 15:36:55 +0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createContext, lazy, useEffect, useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-function App() {
+// import Loyout from './components/Loyout';
+// import Shops from './pages/ShopsPage/Shops';
+
+// const HistoryLazy = lazy(() =>
+//    import('./pages/HistoryPage/History'),
+// );
+// const ProductsLazy = lazy(() =>
+//    import('./components/Products'),
+// );
+// const OrdersLazy = lazy(() =>
+//    import('./pages/OrdersPage/Orders'),
+// );
+
+export const DeliveryContext = createContext(null);
+
+// const orderListStorege =
+//    JSON.parse(
+//       localStorage.getItem('order-list'),
+//    ) || [];
+
+const App = () => {
+  //  const [orderList, setOrderList] = useState(
+  //     orderListStorege,
+  //  );
+
+  //  useEffect(() => {
+  //     localStorage.setItem(
+  //        'order-list',
+  //        JSON.stringify(orderList),
+  //     );
+  //  }, [orderList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <DeliveryContext.Provider
+    //    value={{ setOrderList, orderList }}
+    // >
+    <Routes>
+      {/* <Route
+               path="delivery"
+               element={<Loyout />}
+            >
+               <Route
+                  path="shops"
+                  element={<Shops />}
+               >
+                  <Route
+                     path=":id"
+                     element={<ProductsLazy />}
+                  />
+               </Route> */}
+      {/* <Route
+                  path="orders"
+                  element={<OrdersLazy />}
+               /> */}
+      {/* <Route
+                  path="history"
+                  element={<HistoryLazy />}
+               /> */}
+      {/* </Route> */}
+      <Route path="/*" element={<Navigate to="/delivery/shops" />} />
+    </Routes>
+    // </DeliveryContext.Provider>
   );
-}
+};
 
 export default App;
